ae2c50d42611baab7592bbe7ac5b51868c066c51
nagderpdevops <dgderp-devops@nagarro.com>
2024-03-08 14:47:35 +0530

    first commit

---
+++ src/test/java/com/example/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("hhhhhhhhhhhh");
+	}
+
+}
